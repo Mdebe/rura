201efcbcd20f3f261720167cdf961cd045674b76
mdebe useyiphisi <mfundosithole@outlook.com>
2026-07-07 23:19:18 +0200

    Mazulu

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,11 +1,14 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 pluginManagement {
-    def flutterSdkPath = {
-        def properties = new Properties()
-        file("local.properties").withInputStream { properties.load(it) }
-        def flutterSdkPath = properties.getProperty("flutter.sdk")
-        assert flutterSdkPath != null, "flutter.sdk not set in local.properties"
-        return flutterSdkPath
-    }()
+    val flutterSdkPath = run {
+        val properties = Properties()
+        FileInputStream(file("local.properties")).use { properties.load(it) }
+        val path = properties.getProperty("flutter.sdk")
+        require(path != null) { "flutter.sdk not set in local.properties" }
+        path
+    }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
@@ -17,9 +20,9 @@ pluginManagement {
 }
 
 plugins {
-    id "dev.flutter.flutter-plugin-loader" version "1.0.0"
-    id "com.android.application" version "8.6.0" apply false
-    id "org.jetbrains.kotlin.android" version "1.9.24" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
-include ":app"
+include(":app")
